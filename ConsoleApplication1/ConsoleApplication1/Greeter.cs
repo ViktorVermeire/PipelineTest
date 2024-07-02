110b4e2afad033eb7dac86d58f8224e8c7741d3f
Viktor Louis H Vermeire <viktorlouish.vermeire@dedalus.eu>
2024-07-02 12:34:39 +0200

    build test 1

---
+++ ConsoleApplication1/ConsoleApplication1/Greeter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class Greeter
+    {
+        private string _name;
+        public Greeter(string name)
+        {
+            this._name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello " + _name);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Greeter greeter = new Greeter("Viktor");
+            greeter.Greet();
+        }
+    }
+}
