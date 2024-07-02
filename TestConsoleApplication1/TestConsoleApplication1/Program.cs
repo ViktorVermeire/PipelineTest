289949187d5451bcabc6d6d3e0e77a2a4b46efff
Viktor Louis H Vermeire <viktorlouish.vermeire@dedalus.eu>
2024-07-02 10:53:04 +0200

    add TestConsoleApllication

---
+++ TestConsoleApplication1/TestConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace TestConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
